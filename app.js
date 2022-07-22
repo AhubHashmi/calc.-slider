4fc27d521627d02bebfd4b840eaaa2c2d9bbff92
AhubHashmi <107793320+AhubHashmi@users.noreply.github.com>
2022-07-23 00:22:19 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,21 +1,28 @@
-var screen = document.getElementById("screen");
-var isCalculated;
+let slideIndex = 1;
+showSlides(slideIndex);
 
-function clearAll() {
-    screen.value = "";
+// Next/previous controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
 }
 
-function getBtnValue(val) {
-    if (isCalculated && typeof val == "number") {
-        clearAll();
+// Thumbnail image controls
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
     }
-    screen.value += val;
-    isCalculated = false;
-}
-
-function calculate() {
-    isCalculated = true;
-    var val = screen.value;
-    var finalVal = eval(val);
-    screen.value = finalVal;
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
 }
